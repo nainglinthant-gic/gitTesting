2005d5ae09a473a641051027b62fc0f8dac00dc5
GIC-LPT-0196-User <gic-lpt-0196-user@NLT.local>
2025-02-23 08:45:00 +0630

    create welcome page

---
+++ src/main/java/com/gitEg/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.gitEg.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String welcomePage(Model model) {
+        model.addAttribute("title", "Welcome to Our E-Commerce Store");
+        return "welcome";
+    }
+}
